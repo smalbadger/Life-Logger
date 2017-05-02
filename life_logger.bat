7781d432b733ad7d527c27af3b6e1eebae7aecf7
Sam Badger <smalbadger@email.arizona.edu>
2017-05-01 23:23:54 -0700

    Add files via upload

---
+++ life_logger.bat
@@ -0,0 +1,13 @@
+cls
+@echo opening life logger...
+@echo --------------------------------------
+@echo off
+color B
+@echo on
+@call life_logger.py
+@call journal.csv
+@echo off
+pause
+cls
+color A
+@echo on
